89b9f582a282b3c5bb90878273a6a87aa18f93a4
AidanKuo <akuo18@uw.edu>
2023-10-15 00:51:13 -0700

    edits

--- insync/src/components/Home.js
+++ insync/src/components/Home.js
@@ -10,27 +10,30 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 function Home() {
     return (
         <div>
-            <Navbar className='navbar' data-bs-theme="dark">
+            <Navbar expand="lg" className='navbar' data-bs-theme="light">
             <Navbar.Brand className='margin' as={Link} to="/DubHacks23">
             <img
               src="https://cdn.discordapp.com/attachments/1162859852440289421/1162973763621634148/DUBHACK_LOGO_3.png?ex=653de2b8&is=652b6db8&hm=62045bf78626f440076852abc92ba338991f71ca1ecb599a2e98b2d196b77b28&"
-              width="25"
-              height="30"
+              width="100"
+              height="150"
               className="d-inline-block align-top"
               alt="bruh"
             />
           </Navbar.Brand>
-            <Navbar.Brand className='left-text' as={Link} to="/DubHacks23">InSync</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className='left-text'>
+                <Nav.Link as={Link} to="/DubHacks23">InSync</Nav.Link>
+                </Nav>
               <Nav className="ms-auto">
-              <Nav.Link className='right-text' as={Link} to="/DubHacks23">Home</Nav.Link> 
+              <Nav.Link as={Link} to="/DubHacks23">Home</Nav.Link> 
                 <Nav.Link className='right-text' as={Link} to="/about">About</Nav.Link>
               </Nav>
             </Navbar.Collapse>
         </Navbar>
         <div className='graphic-container'>
-            <p className=''>ayo</p>
+            <p className='desc-title-text'>InSync</p>
+            <p className='desc-text'>talk to fweiedns</p>
         </div>
         <div className='desc-container'>
             <img
